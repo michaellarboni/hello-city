1cd1e7c3382d296d5d5eb48dcfc07323d7eed53d
mike <mike2mars@hotmail.com>
2020-09-20 11:35:36 +0200

    Add about page, images an styling with Tailwind

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -9,10 +9,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class PagesController extends AbstractController
 {
     /**
-     * @Route("/pages", name="app_home")
+     * @Route("/", name="app_home")
      */
     public function home() : Response
     {
         return $this->render('pages/home.html.twig');
+    }
+
+    /**
+     * @Route("/about-us", name="app_about")
+     */
+    public function about() : Response
+    {
+        return $this->render('pages/about.html.twig');
     }
 }
